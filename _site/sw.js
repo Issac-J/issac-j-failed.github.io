245eb496e6f0c576c61dfe9d60a82f582ecbf540
EggsyJ <fordev0905@gmail.com>
2023-11-28 21:55:31 +0900

    after bundle commands

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231128.214950';
+const cacheName = 'chirpy-20231128.215456';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
